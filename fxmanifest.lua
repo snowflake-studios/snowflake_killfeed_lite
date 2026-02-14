4d56897931e8bc3bb39f9ffadab18efc6a29999b
snowflake-studios <snowflakestudios.dev@gmail.com>
2026-02-13 18:31:20 -0800

    v1.0.1: flatten repo structure + added Cross-Framework Name Resolution

---
+++ fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Snowflake Studios'
+description 'Snowflake Killfeed Lite - Free Edition'
+version '1.0.1'
+
+-- Dependencies
+dependencies {
+    'ox_lib',
+    'baseevents'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+escrow_ignore {
+    'config.lua',
+    'client/*.lua',
+    'server/*.lua',
+    'html/*'
+}
